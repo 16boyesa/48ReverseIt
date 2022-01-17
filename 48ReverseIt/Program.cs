0e21826ee0f3c74ba5764e0ed6315d1aad106dc0
Adam Boyes <16BoyesA@fulford.york.sch.uk>
2022-01-17 13:41:47 +0000

    Updated

--- 48ReverseIt/Program.cs
+++ 48ReverseIt/Program.cs
@@ -8,8 +8,19 @@ namespace _48ReverseIt
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main() { 
+        string text = "12345", a, b;
+            for (int i = 0; i < (text.Length+1) / 2; i ++) { 
+                a = text.Substring(i, 1);
+                b = text.Substring(text.Length - i, 1);
+                text.Substring(i, 1);
+            }
         }
     }
 }
+
+
+// https://live.withcode.uk/alevel/?id=48
+// 48 - Reverse It
+
+// Have the programme allow a user to enter some text and then the programme will reverse it and print it back to the screen.
