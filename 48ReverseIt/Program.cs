b3b9bca0231a39dfbd568b7fdbaa1ba43493fdf9
Adam Boyes <16BoyesA@fulford.york.sch.uk>
2022-01-19 09:49:00 +0000

    Completed a main function tasks

--- 48ReverseIt/Program.cs
+++ 48ReverseIt/Program.cs
@@ -1,26 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _48ReverseIt
-{
-    class Program
-    {
+namespace _48ReverseIt { 
+    class Program { 
         static void Main() { 
-        string text = "12345", a, b;
-            for (int i = 0; i < (text.Length+1) / 2; i ++) { 
-                a = text.Substring(i, 1);
-                b = text.Substring(text.Length - i, 1);
-                text.Substring(i, 1);
-            }
-        }
+            string text = Console.ReadLine();
+            for (int i = 0; i < text.Length; i++) { 
+            Console.Write(text.Substring(text.Length - (i + 1), 1)); }
+            Console.Read(); }
     }
 }
 
-
 // https://live.withcode.uk/alevel/?id=48
-// 48 - Reverse It
-
-// Have the programme allow a user to enter some text and then the programme will reverse it and print it back to the screen.
